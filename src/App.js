860dfbe6e1767d2c235f21c1b889f02d23f31fb8
nurullahbaykara <nurullahbaykara017@gmail.com>
2023-07-30 14:25:46 +0300

    load

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
-import React from "react";
+import React, { useEffect } from "react";
 
 function App() {
-  return (
-    <div>
 
+  useEffect(()=> {
+    if(process.env.NODE_ENV === 'production') {
+      // analytici bilgilendir
+    }
+  }, [])
+
+  return (
+    <div className="App">
+      <h3>{process.env.NODE_ENV}</h3>
+     <p>
+      {
+        process.env.REACT_APP_API_URL
+      }
+     </p>
     </div>
   );
 }
